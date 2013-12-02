909fbd08dc25c92cc8054fc7a3837853076a44c0
Ivan Pacheco <ivan@sovietika.com>
2013-12-02 11:51:07 -0600

    Terminados tooltips. Agregado nuevo marcado para pestañas (se hizo solo)

--- js/plugins.js
+++ js/plugins.js
@@ -2,6 +2,14 @@
 var $page = $('body'),
 	$window = $(window);
 
+// Tabs
+function Tabs (setting) {
+	this.init();
+}
+Tabs.prototype.init = function __init__ () {
+
+};
+
 // Toogle
 function Toggle (settings) {
 	this.button = $(settings.button),
@@ -29,30 +37,59 @@ Toggle.prototype.init = function __init__ () {
 
 // Tooltip
 function Tooltip (settings) {
-	this.element = $(settings.element);
+	this.element = $(settings.element),
+	this.id = settings.id,
+	this.tooltip = $('<div />', { id: this.id });
 	this.init();
 }
+Tooltip.prototype.getPosition = function getPosition (event) {
+	return { x: event.clientX, y: event.clientY };
+};
 Tooltip.prototype.hide = function hide (event) {
 	var _this = event.data.context;
+	_this.tooltip.detach();
+};
+Tooltip.prototype.move = function move (event) {
+	var _this = event.data.context,
+		position = _this.getPosition(event);
+	_this.tooltip.css({ left: position.x + 2, top: position.y + 2 });
 };
 Tooltip.prototype.show = function show (event) {
 	var _this = event.data.context,
-		positionX = event.clientX,
-		positionY = event.clientY;
-	$page.append($('<div />', { id: 'tooltip' }));
+		position = _this.getPosition(event);
+	$page.append(_this.tooltip
+		.text($(this).data('title'))
+		.css({ left: position.x, top: position.y }));
+};
+Tooltip.prototype.title = function title () {
+	var element = $(this);
+	element
+		.data('title', this.title)
+		.removeAttr('title');
 };
 Tooltip.prototype.init = function __init__ () {
 	this.element
 		.on('mouseenter', { context: this }, this.show)
 		.on('mouseleave', { context: this }, this.hide)
-		.on('mousemove', { context: this }, this.move);
+		.on('mousemove', { context: this }, this.move)
+		.each(this.title);
 };
 
 function start () {
-	var toggle = new Toggle({ button: 'dt', clss: 'current', fx: 'slideToggle', toggle: 'dd', speed: 200 });
+	var tabs = new Tabs({  });
+	var toggle = new Toggle({ button: '.toggle dt', clss: 'current', fx: 'slideToggle', toggle: 'dd', speed: 200 });
 	var tooltip = new Tooltip({
-		element: '.tooltip'
+		element: '.tooltip',
+		id: 'tooltip'
+	});
+	var otrotooltip = new Tooltip({
+		element: '.otrotooltip',
+		id: 'otrotooltip'
 	});
 }
 
 $(document).ready(start);
+
+
+
+
